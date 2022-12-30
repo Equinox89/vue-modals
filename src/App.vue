c4684b0babae9bc02d865c5db55f47432c486469
oleh <mastercrutor89@gmail.com>
2022-12-30 23:02:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <h1>Hello modals</h1>
+          <button class="btn btnPrimary" @click="showFirstModal = !showFirstModal">Open first modal</button>
+          <Modal title="First modal" v-if="showFirstModal" @close="showFirstModal = !showFirstModal"></Modal>
+        </div>
+      </section>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+import Modal from "@/components/Modal.vue";
+
+export default {
+  components: {Modal},
+
+  data() {
+    return {
+      showFirstModal: false
+    }
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+}
+</style>
